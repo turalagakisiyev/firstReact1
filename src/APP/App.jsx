56279fd9a205a44dfd3caf35cea8e2f3f800489a
turalagakisiyev <114071593+turalagakisiyev@users.noreply.github.com>
2022-11-22 14:25:57 +0400

    complete

---
+++ src/APP/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    return (
+        <>
+            <div>
+                <h3>Tural Agakishiyev</h3>
+                <p>Now I can create component</p>
+            </div>
+
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+
+class Input extends Component{
+    render(){
+        return(
+            <h2><input type="text" placeholder="Heisenberg"/> </h2>
+        )
+    }
+}
+export default Input
